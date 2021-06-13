49236ad2c62d4ec36b2198d9b1bbc8d2cf24b5af
DanielaG0627 <danielagb1790@gmail.com>
2021-06-13 18:14:21 -0400

    Created retirement_info table and exported file

--- schema.sql
+++ schema.sql
@@ -54,4 +54,22 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM ;
+-- Select employees for potential retirement
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Look for employees born on 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Create list of employees for export
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Verify file count
+SELECT COUNT(*) FROM retirement_info
