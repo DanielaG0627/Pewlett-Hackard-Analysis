e949c98f5fa9be853516b9e1538543e8156be0b0
DanielaG0627 <danielagb1790@gmail.com>
2021-06-13 22:03:12 -0400

    Completed Pewlett Hackard Analysis

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Pewlett-Hackard-Analysis
+
+## Overview
+Pewlett Hackard will soon face a "silver tsunami", the baby boomer employee population is close to retirement. This analysis was made to help Pewlett Hackard determine how many roles will be vacant per department and job title as a result of the retirement wave and the possibility of a mentorship program in order to prepare for what's to come.
+
+## Results
+Provide a bulleted list with four major points from the two analysis deliverables. Use images as support where needed.
+
+After conducting the analysis, the following was found:
+
+- The total number of retiring employees is 90,398. This was found by determining the number of employees who were born between January 1, 1952 and December 31, 1955. 
+
+- 30% of Pewlett Hackard employees are baby boomers.
+
+- The table below shows a breakdown of the number of retirement-age employees by job title. 
+
+- There are 1,549 employees who were born in 1965 that qualify for the Mentorship program to work part-time and mentor new employees.
+
+## Summary
+
+- How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+
+ 90,398 roles will need to be filled.
+
+- Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+
+ At 1,549 employees who are of retirement age and were born in 1965, I believe there are enough mentors for the new employees.
+
+- The table below shows a breakdown of the sum of salaries of the retiring employees by title.
+
+- The table below shows a breakdown of the number of retirement-age employees by department.
+
--- schema.sql
+++ schema.sql
@@ -65,7 +65,7 @@ FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
 
 -- Create list of employees for export
-SELECT first_name, last_name
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
@@ -73,3 +73,62 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Verify file count
 SELECT COUNT(*) FROM retirement_info
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT COUNT (*) FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('1999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO ret_by_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY dept_no;
+
+-- Employee information
+SELECT emp_no, last_name, first_name, gender, salary
+
+
+-- Management
+SELECT dept_no, dept_name, emp_no, last_name, first_name, from_date, to_date
+
+-- Department Retirees 
+--SELECT current_emp with employee's departments.
+
